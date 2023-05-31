beca2fa09bb49f382d6b26aa5dc5322bc2b0995b
Oneluv1 <adegunju27@gmail.com>
2023-05-31 17:46:05 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FetchData from './FetchData';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+
+function App () {
+  return (
+    <FetchData />
+  )
+}
+
+
+
+export default App;
